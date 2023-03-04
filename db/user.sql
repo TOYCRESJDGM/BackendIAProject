1e20eec7cc8a9e40014074e9e3ff33f0479c8edc
TOYCRESJDGM <juandavid00412@gmail.com>
2023-03-03 23:26:33 -0500

    add sql user

---
+++ db/user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE if not exists User (
+    id int(11) NOT NULL auto_increment COMMENT 'identificador',
+    userName varchar(255) NOT null COMMENT 'Nombre de usuario',
+    email varchar(255) NOT null COMMENT 'correo electronico',
+    password varchar(255) NOT null COMMENT 'contraseña',
+    creationDate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Fecha de creacion',
+    ModificationDate timestamp COMMENT 'Fecha de modificacion',
+    PRIMARY KEY (id)
+) auto_increment=1 default CHARSET=utf8mb4 comment 'user';
